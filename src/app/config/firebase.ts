685e1cd2727f484ce02876550a527bbeadf8af2c
Taka Sasaki <tsukune2339@gmail.com>
2018-08-17 11:38:15 +0900

    Read .env from external file, and put Firebase env vars.

--- src/app/config/firebase.ts
+++ src/app/config/firebase.ts
@@ -2,16 +2,20 @@ import firebase from 'firebase'
 import 'firebase/auth'
 import 'firebase/firestore'
 
+const options = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  messagingSenderId: process.env.FIREBASE_MESSAGEGING_SENDER_ID,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+}
+
 export const app: firebase.app.App = !firebase.apps.length
-  ? firebase.initializeApp({
-      apiKey: '<apiKey>',
-      authDomain: '<authDomain>',
-      databaseURL: '<databaseURL>',
-      projectId: '<projectId>',
-      storageBucket: '<storageBucket>'
-      // messagingSenderId: '<messagingSenderId>'
-    })
+  ? firebase.initializeApp(options)
   : firebase.app()
+
+console.log(app.options)
 
 const firestore = app.firestore()
 firestore.settings({ timestampsInSnapshots: true })
--- src/app/next.config.js
+++ src/app/next.config.js
@@ -1,2 +1,19 @@
+require('dotenv').config()
+const path = require('path')
+const Dotenv = require('dotenv-webpack')
+
 const withTypescript = require('@zeit/next-typescript')
-module.exports = withTypescript({ distDir: '../../dist/functions/next' })
+module.exports = withTypescript({
+  distDir: '../../dist/functions/next',
+  webpack(config) {
+    config.plugins = config.plugins || []
+    config.plugins = [
+      ...config.plugins,
+      new Dotenv({
+        path: path.join(__dirname, '../../.env'),
+        systemvars: true
+      })
+    ]
+    return config
+  }
+})
